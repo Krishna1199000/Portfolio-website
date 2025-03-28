bb9e6ffa89e2354d8de4a2524d39586ff49b8d57
krishna <gohilkrishna9004@gmail.com>
2025-03-28 08:45:32 +0530

    Make some changes

--- project/src/data/projects.ts
+++ project/src/data/projects.ts
@@ -10,22 +10,22 @@ export const projects = [
     title: "AI Interview Prep",
     description: "Voice-enabled AI assistant for interview preparation and feedback",
     image: "https://images.unsplash.com/photo-1596524430615-b46475ddff6e?auto=format&fit=crop&q=80",
-    link: "#",
-    category: "https://github.com/Krishna1199000/ai-voice-agent-for-interview-prep"
+    link: "https://github.com/Krishna1199000/ai-voice-agent-for-interview-prep",
+    category: "Full stack with Ai agent"
   },
   {
     title: "Uber Clone",
     description: "Real-time ride-sharing platform with live tracking and payment integration",
     image: "https://images.unsplash.com/photo-1549194898-60fd030ecc0f?auto=format&fit=crop&q=80",
-    link: "#",
-    category: "https://github.com/Krishna1199000/Ola-clone"
+    link: "https://github.com/Krishna1199000/Ola-clone",
+    category: "Mern stack"
   },
   {
     title: "PayTM Clone",
     description: "Digital payment solution with UPI integration and wallet features",
     image: "https://images.unsplash.com/photo-1556742049-0cfed4f6a45d?auto=format&fit=crop&q=80",
-    link: "#",
-    category: "https://github.com/Krishna1199000/Ola-clone"
+    link: "https://github.com/Krishna1199000/Ola-clone",
+    category: "Mern stack"
   },
   {
     title: "Fashion Store",
